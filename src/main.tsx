44fc566046fbb30756d545fb3249c6c0b24da9c5
Prakhar Raj <prakharraj001@gmail.com>
2024-12-06 07:01:19 +0530

    github pages deployment configs

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="/" element={<Home />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router";
+import logoImgSrc from "/sannjh-logo-normal.png";
 
 function Header() {
   return (
     <>
       <header className="relative z-20 flex flex-col items-start justify-between bg-pink-50/50 px-[5dvw] py-4 md:flex-row md:flex-wrap md:items-center md:bg-transparent">
         <div className="flex items-center">
-          <img
-            className="h-12 w-auto"
-            src="/sannjh-logo-normal.png"
-            alt="Saanjh Logo"
-          />
+          <img className="h-12 w-auto" src={logoImgSrc} alt="Saanjh Logo" />
           <div className="text-2xl font-bold text-gray-600 md:text-white">
             Saanjh
           </div>
